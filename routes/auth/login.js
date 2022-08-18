d24924389076c287ad856e0fc5de6e64d02ae26f
mbistami <mbistamimoussa@gmail.com>
2022-08-18 02:12:54 +0100

    fix

--- routes/auth/login.js
+++ routes/auth/login.js
@@ -29,7 +29,7 @@ router.post("/", async (req, res, next) => {
         jwt.sign(...collection, process.env.SECRET_TOKEN, {
           expiresIn: "1800s",
         }),
-        { maxAge: 900000, domain: ".shinyrp.dk" }
+        { maxAge: 900000, domain: ".herokuapp.com", secure: true }
       );
       res.send();
       return;
